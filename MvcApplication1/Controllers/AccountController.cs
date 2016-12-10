8def098a906f3ae596429128ad3d1dfe7e831b46
Dhruv9979 <starb9979@gmail.com>
2016-12-09 22:50:10 -0500

    Add project files.

---
+++ MvcApplication1/Controllers/AccountController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+
+
+
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+
+            MVCTestEntities _entity = new MVCTestEntities();
+
+           var _result =  _entity.Users.Select(x => x.Email == user.Email && x.Pws == user.Pws).ToList();
+
+           if (_result[0])
+           {
+               return RedirectToAction("Index", "Shopping");
+
+           }
+           else
+           {
+
+               return View();
+           }
+        }
+
+
+    }
+}
---
+++ MvcApplication1/Controllers/ShoppingController.cs
@@ -0,0 +1,46 @@
+﻿using MvcApplication1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        //
+        // GET: /Shopping/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Product
+        [HttpGet]
+        public ActionResult Products(int ProductID)
+        {
+
+            MVCTestEntities _entity = new MVCTestEntities();
+            List<Product> hh = _entity.Products.Where(x => x.CategoryID == ProductID).ToList();
+
+          return View(hh);
+        }
+
+
+        [HttpGet]
+        public ActionResult ProductDetails(int ProductID)
+        {
+
+            MVCTestEntities _entity = new MVCTestEntities();
+            Product hh = _entity.Products.Where(x => x.ProductID == ProductID).First();
+
+            return View(hh);
+        }
+
+
+
+    }
+}
